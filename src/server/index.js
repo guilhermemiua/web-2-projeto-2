c00a15870dd829e32c272a97c91ac10ec6d85a75
guilhermemiua <guilhermemiua@gmail.com>
2021-05-16 20:08:20 -0300

    fix: general adjustments

--- src/server/index.js
+++ src/server/index.js
@@ -8,6 +8,6 @@ require('dotenv').config({
       : path.join(__dirname, '../../.env'),
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is listening on port: ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 3333, () => {
+  console.log(`Server is listening on port: ${process.env.PORT || 3333}`);
 });
